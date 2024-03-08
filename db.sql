fd5055b71db180e76f79990551da2aed18011dcd
mhdmd01 <mouadmahdi01@gmail.com>
2024-03-09 00:38:10 +0100

    inizion db

--- db.sql
+++ db.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS utenti;
 DROP TABLE IF EXISTS recnsioni;
 DROP TABLE IF EXISTS sogni;
+DROP TABLE IF EXISTS acquisti;
+DROP TABLE IF EXISTS chipOrders;
+DROP TABLE IF EXISTS scissionOrders;
 
 
 CREATE TYPE Categorie AS ENUM (
@@ -35,8 +38,9 @@ CREATE TABLE recnsioni (
   user VARCHAR(20) NOT NULL,
   testo VARCHAR(256) NOT NULL,
   sogno VARCHAR(20),
+  data_ins TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-  FOREIGN KEY (user) REFERENCES utenti(user_name) ON DELETE CASCADE
+  FOREIGN KEY (user) REFERENCES utenti(user_name) ON DELETE CASCADE,
   FOREIGN KEY (sogno) REFERENCES sogni(titolo) ON DELETE CASCADE
 );
 
@@ -46,4 +50,40 @@ CREATE TABLE  sogni(
   prezzo INT NOT NULL, 
   categoria Categorie, 
   path VARCHAR(20) NOT NULL
+  data_ins TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
+
+CREATE TABLE acquisti(
+  user VARCHAR(20) NOT NULL,
+  titolo VARCHAR(20) NOT NULL,
+  PRIMARY KEY (user, titolo),
+
+  FOREIGN KEY (user) REFERENCES utenti(user_name) ON DELETE CASCADE,
+  FOREIGN KEY (sogno) REFERENCES sogni(titolo) ON DELETE CASCADE
+);
+
+CREATE TABLE chipOrders(
+  user VARCHAR(20) PRIMARY KEY,
+  nome VARCHAR(20) NOT NULL,
+  cognome VARCHAR(20) NOT NULL,
+  età INT NOT NULL,
+  mail VARCHAR(20) NOT NULL,
+  telefono VARCHAR(20),
+  messaggio VARCHAR(256),
+
+  FOREIGN KEY (user) REFERENCES utenti(user_name) ON DELETE CASCADE,
+)
+
+CREATE TABLE scissionOrders(
+  user VARCHAR(20) PRIMARY KEY,
+  nome VARCHAR(20) NOT NULL,
+  cognome VARCHAR(20) NOT NULL,
+  età INT NOT NULL,
+  mail VARCHAR(20) NOT NULL,
+  telefono VARCHAR(20),
+  nome_azienda VARCHAR(32),
+  indirizzo(128),
+  messaggio VARCHAR(256),
+
+  FOREIGN KEY (user) REFERENCES utenti(user_name) ON DELETE CASCADE,
+)
