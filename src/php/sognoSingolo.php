99f14d83497c0f7ac6087a6cc8232b29595b7966
Davide Picello <119404292+DavidePicc@users.noreply.github.com>
2024-03-12 10:01:27 +0100

    Aggiunte gestioni errori su tutte le query

--- src/php/sognoSingolo.php
+++ src/php/sognoSingolo.php
@@ -12,21 +12,25 @@
     }
 
     $risultato = $functions->executeQuery("SELECT * FROM sogni WHERE titolo=\"$sogno\";");
+
     
     $pagina = new newPage("../html/sognoSingolo.html", 
                             $sogno, 
                             strval($sogno)." saudade", 
                             "Pagina riguardante ".strval($sogno));
-    
-    if(mysqli_num_rows($risultato) > 0){
-        foreach( $risultato as $row){
-            $pagina->modificaHTML("{titolo}", $row['titolo']);
-            $pagina->modificaHTML("{descrizione}", $row['descrizione']);
-            $pagina->modificaHTML("{prezzo}", $row['prezzo']);
-            $pagina->modificaHTML("{pathImg}", $row['pathImg']);
+
+    if($risultato == null)
+        $pagina->printErrorPage("Sogno non trovato, tornare alla pagina <a href=\"sogni.php\">Sogni</a> e provare a riselezionare il sogno desiderato");                            
+    else{
+        if(mysqli_num_rows($risultato) > 0){
+            foreach( $risultato as $row){
+                $pagina->modificaHTML("{titolo}", $row['titolo']);
+                $pagina->modificaHTML("{descrizione}", $row['descrizione']);
+                $pagina->modificaHTML("{prezzo}", $row['prezzo']);
+                $pagina->modificaHTML("{pathImg}", $row['pathImg']);
+            }
         }
-    }else{
-        $pagina->printErrorPage("Sogno non trovato, tornare alla pagina <a href=\"sogni.php\">Sogni</a> provare a riselezionare il sogno desiderato");
     }
+    
 
     $pagina->printPage();
