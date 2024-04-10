802485a91c3862920f1532a806e20a009b5994fe
mhdmd01 <mouadmahdi01@gmail.com>
2024-04-10 03:18:12 +0200

    barra di ricerca

--- src/php/sogni.php
+++ src/php/sogni.php
@@ -6,10 +6,17 @@
 
     use functions\functions;
     $functions = new functions();
-    $sogni = $functions->executeQuery("SELECT * FROM sogni;");
+
+    //BARRA DI RICERCA
+    $item = isset($_GET['search']) ? $functions->pulisciInput($_GET['search']) : '';
+    $sql = "SELECT * FROM sogni WHERE titolo LIKE '%$item%' OR descrizione LIKE '%$item%'";
+    $sogni = $functions->executeQuery($sql);
+
+    //Vecchia select per all
+    //$sogni = $functions->executeQuery("SELECT * FROM sogni;");
 
     if($sogni == null)
-        $pagina->printErrorPage("Non ci sono sogni disponibili al momento, riprovare più tardi o tornare alla pagina <a href=\"index.php\">home</a>");
+        $pagina->printErrorPage("Non ci sono sogni disponibili al momento o per la ricerca fatta, riprovare più tardi o tornare alla pagina <a href=\"index.php\">home</a>");
     else{
         $annuncio = "";
 
@@ -22,7 +29,7 @@
             $annuncio = str_replace("{prezzo}", $row['prezzo'], $annuncio);
             $annuncio = str_replace("{pathImg}", "\"../assets/sogni/".$row['titolo'].".".$row['estensioneFile']."\"", $annuncio);
         }
-
+        $pagina->modificaHTML("{ricerca}", $item);
         $pagina->modificaHTML("{elencoSogni}", $annuncio);
     }
 
