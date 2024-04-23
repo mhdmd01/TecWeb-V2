7da99f9cdcaa373fbe3446e7045c1d5767c023a7
Davide Picello <119404292+DavidePicc@users.noreply.github.com>
2024-04-23 11:22:26 +0200

    Miglioramento dashboardAdmin ma ancora bozza

--- src/php/dashboardUser.php
+++ src/php/dashboardUser.php
@@ -1,19 +1,45 @@
 <?php
     require_once("newPage.php");
+    require_once("functions.php");
 
-    $pagina = new newPage("../html/dashboardAdmin.html", "Area personale admin", "Area personale admin", "Area personale admin Saudade");
+    use functions\functions;
+    $functions = new functions();
+    $functions->openDBConnection();
+
+    $pagina = new newPage("../html/dashboardUser.html", "Area personale user", "Area personale user", "Area personale user Saudade");
 
     if(isset($_SESSION['user_name'])){
-        if($_SESSION['user_name'] === "admin") { 
-            $pagina->modificaHTML("{placeholder}",  "<div><a href='aggiungiSogno.php'>Aggiungi sogno</a></div>");
-            $pagina->printPage();
-        }
-        $pagina = new newPage("../html/dashboardUser.html", "Area personale", "Area personale", "Area personale Saudade");
-        $pagina->modificaHTML("{elencosogni}", "HELLO" );
-        $pagina->printPage();
-    }else{
-        $pagina->modificaHTML("{placeholder}",  "<div>Pagina riservata, accedere come utente prima</div>");
-        $pagina->printPage();
-    }
-
+        if($_SESSION['user_name'] != "admin") { 
+            //Verifico se esiste utente (improbabile)
+            $stmt = $functions->getConnection()->prepare("SELECT * FROM utenti WHERE user_name=?");
+            $stmt->bind_param("s", $_SESSION['user_name']);
+            $stmt->execute();
+            $risultato = $stmt->get_result();
     
+            if(mysqli_num_rows($risultato) > 0){
+                $stmt = $functions->getConnection()->prepare("SELECT * FROM acquisti WHERE user_name=?");
+                $stmt->bind_param("s", $_SESSION['user_name']);
+                $stmt->execute();
+                $ris = $stmt->get_result();
+    
+                if(mysqli_num_rows($ris) > 0){
+                    $acquisti = "";
+                    while ($row = $ris->fetch_assoc()) {
+                        $acquisti .= "<br>";    //Da eliminare
+                        $acquisti .= $row['articolo']. " ";
+                        $acquisti .= $row['data'];
+                    }
+                    $pagina->modificaHTML("{elencosogni}", $acquisti);
+                }else{
+                    $pagina->modificaHTML("{elencosogni}", "Nessun acquisto ancora effettuato");
+                }
+            }else{
+                $pagina->printErrorPage("Utente non trovato"); //Quasi impossibile accada
+            }
+        }
+    }else{
+        $pagina->printErrorPage("Pagina riservata ad utenti loggati, esegui il <a href=\"login.php\">login</a> per accedere");
+    }    
+
+    $functions->closeConnection();
+    $pagina->printPage();
