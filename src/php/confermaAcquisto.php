4f0479512467730db293ae7a4476a70190e3fff6
mhdmd01 <mouadmahdi01@gmail.com>
2024-05-25 15:15:52 +0200

    fix pagine login

--- src/php/confermaAcquisto.php
+++ src/php/confermaAcquisto.php
@@ -4,45 +4,55 @@ require_once("functions.php");
 
 use functions\functions;
 
+$pagina = new newPage("../html/sognoNonTrovato.html", "Sogno non disponibile", "Sogno non disponibile", "Pagina di errore per il sogno non disponibile");
+
+
 $functions = new functions();
 $functions->openDBConnection();
 
-if (isset($_GET['sogno'])) {
-    $sogno = urldecode($_GET['sogno']);
+if(isset($_SESSION['user_name'])){
+    if (isset($_GET['sogno'])) {
+        $sogno = urldecode($_GET['sogno']);
 
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        } // ???
 
-    // Verifica se l'utente ha già effettuato l'acquisto
-    $stmt = $functions->getConnection()->prepare("SELECT * FROM acquisti WHERE user_name=? AND articolo=?");
-    $stmt->bind_param("ss", $_SESSION['user_name'], $sogno);
-    $stmt->execute();
-    $risultato = $stmt->get_result();
-
-    if (mysqli_num_rows($risultato) == 0) {
-        // Registra l'acquisto se non è stato ancora effettuato
-        $stmt = $functions->getConnection()->prepare("INSERT INTO acquisti (user_name, articolo) VALUES (?, ?)");
+        // Verifica se l'utente ha già effettuato l'acquisto
+        $stmt = $functions->getConnection()->prepare("SELECT * FROM acquisti WHERE user_name=? AND articolo=?");
         $stmt->bind_param("ss", $_SESSION['user_name'], $sogno);
         $stmt->execute();
+        $risultato = $stmt->get_result();
 
-        // Prepara i dati per la pagina di conferma
-        $titolo = $sogno;
-        $username = $_SESSION['user_name'];
+        if (mysqli_num_rows($risultato) == 0) {
+            // Registra l'acquisto se non è stato ancora effettuato
+            $stmt = $functions->getConnection()->prepare("INSERT INTO acquisti (user_name, articolo) VALUES (?, ?)");
+            $stmt->bind_param("ss", $_SESSION['user_name'], $sogno);
+            $stmt->execute();
 
-        // Carica il contenuto della pagina HTML di conferma acquisto
-        $pagina = new newPage("../html/confermaAcquisto.html", "Conferma acquisto $titolo", $titolo, "Pagina di conferma acquisto per $titolo");
-        $pagina->modificaHTML("{username}", $username);
-        $pagina->modificaHTML("{titolo}", $titolo);
+            // Prepara i dati per la pagina di conferma
+            $titolo = $sogno;
+            $username = $_SESSION['user_name'];   
+
+            // Carica il contenuto della pagina HTML di conferma acquisto
+            $pagina = new newPage("../html/confermaAcquisto.html", "Conferma acquisto $titolo", $titolo, "Pagina di conferma acquisto per $titolo");
+            $pagina->modificaHTML("{username}", $username);
+            $pagina->modificaHTML("{titolo}", $titolo);
+            
+            // Chiudi la connessione al database
+            $functions->closeConnection();
+        } else {
+            $pagina = new newPage("../html/acquistoGiaEffettuato.html", "Sogno già acquistato", "Sogno già acquistato", "Pagina di errore per il sogno non disponibile");
+        }
     } else {
-        $pagina = new newPage("../html/acquistoGiaEffettuato.html", "Sogno già acquistato", "Sogno già acquistato", "Pagina di errore per il sogno non disponibile");
+        $pagina = new newPage("../html/sognoNonTrovato.html", "Sogno non disponibile", "Sogno non disponibile", "Pagina di errore per il sogno non disponibile");
     }
-} else {
-    $pagina = new newPage("../html/sognoNonTrovato.html", "Sogno non disponibile", "Sogno non disponibile", "Pagina di errore per il sogno non disponibile");
+
+}else{
+    $pagina->printErrorPage("Pagina riservata all'<a href=\"login.php\">accedi prima<a>");
 }
 
-// Chiudi la connessione al database
-$functions->closeConnection();
+
 
 // Stampare la pagina HTML con i contenuti dinamici inseriti
 $pagina->printPage();
